231519fc27da75c1cdf465b5dd7f0a9e2cb32874
japhethh <boknoy32130@gmail.com>
2025-04-04 11:26:20 +0800

    fix: handle case when no users are found in GET request

--- frontend/app/api/users/[id]/route.ts
+++ frontend/app/api/users/[id]/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 
-export async function GET(
-) {
-  const users = await prisma.user.findMany();
 
+export const GET = async () => {
+  const users = await prisma.user.findMany()
+  if (!users) {
+    return NextResponse.json({ message: "Users not found!" })
+  }
   return NextResponse.json(users)
+
 }
